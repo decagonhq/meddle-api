c17357e087df478b3d9d3bfd1b6c288bb04eb09e
David Odohi <odohi.david@gmail.com>
2022-07-25 12:05:40 +0100

    refactor logout

--- server/auth_handlers.go
+++ server/auth_handlers.go
@@ -1,11 +1,11 @@
 package server
 
 import (
-	"github.com/decagonhq/meddle-api/errors"
-	"github.com/decagonhq/meddle-api/models"
 	"log"
 	"net/http"
-	"time"
+
+	"github.com/decagonhq/meddle-api/errors"
+	"github.com/decagonhq/meddle-api/models"
 
 	"github.com/decagonhq/meddle-api/server/response"
 	"github.com/gin-gonic/gin"
@@ -13,31 +13,7 @@ import (
 
 func (s *Server) handleSignup() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
-		if tokenI, exists := c.Get("access_token"); exists {
-			if userI, exists := c.Get("user"); exists {
-				if user, ok := userI.(*models.User); ok {
-					if accessToken, ok := tokenI.(string); ok {
-						accBlacklist := &models.BlackList{
-							Email:     user.Email,
-							Model: models.Model{CreatedAt: time.Now().Unix()},
-							Token:     accessToken,
-						}
-
-						err := s.AuthRepository.AddToBlackList(accBlacklist)
-						if err != nil {
-							log.Printf("can't add access token to blacklist: %v\n", err)
-							response.JSON(c, "logout failed", http.StatusInternalServerError, nil, errors.New("can't add access token to blacklist", http.StatusInternalServerError))
-							return
-						}
-						response.JSON(c, "logout successful", http.StatusOK, nil, nil)
-						return
-					}
-				}
-			}
-		}
-		response.JSON(c, "", http.StatusInternalServerError, nil, errors.New("can't get info from context", http.StatusInternalServerError))
-		return
+		response.JSON(c, "successful", http.StatusOK, nil, nil)
 	}
 }
 
@@ -48,31 +24,51 @@ func (s *Server) handleLogin() gin.HandlerFunc {
 	}
 }
 
+func GetClaimsFromToken(c *gin.Context) (string, *models.User, *errors.Error) {
+	var tokenI, userI interface{}
+	var tokenExists, userExists bool
+
+	if tokenI, tokenExists = c.Get("access_token"); !tokenExists {
+		// response.JSON(c, "", http.StatusForbidden, nil, errors.New("forbidden", http.StatusForbidden))
+		return "", nil, errors.New("forbidden", http.StatusForbidden)
+	}
+	if userI, userExists = c.Get("user"); !userExists {
+		// response.JSON(c, "", http.StatusForbidden, nil, errors.New("forbidden", http.StatusForbidden))
+		return "", nil, errors.New("forbidden", http.StatusForbidden)
+	}
+
+	token, ok := tokenI.(string)
+	if !ok {
+		return "", nil, errors.New("internal server error", http.StatusInternalServerError)
+	}
+	user, ok := userI.(*models.User)
+	if !ok {
+		return "", nil, errors.New("internal server error", http.StatusInternalServerError)
+	}
+
+	return token, user, nil
+}
+
 func (s *Server) handleLogout() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenI , _:= c.Get("access_token")
-		userI, _ := c.Get("user")
-		 if tokenI == "access_token" && userI == "user" {
-			 return
-		 }
-		user := userI.(*models.User)
-		if accessToken, ok := tokenI.(string); ok {
-			accBlacklist := &models.BlackList{
-				Email:     user.Email,
-				Token:     accessToken,
-				Model:  models.Model{CreatedAt: time.Now().Unix()},
-				}
-				err := s.AuthRepository.AddToBlackList(accBlacklist)
-				if err != nil {
-					log.Printf("can't add access token to blacklist: %v\n", err)
-					response.JSON(c, "logout failed", http.StatusInternalServerError, nil, errors.New("can't add access token to blacklist", http.StatusInternalServerError))
-						return
-					}
-					response.JSON(c, "logout successful", http.StatusOK, nil, nil)
-					return
-			}
-		response.JSON(c, "", http.StatusInternalServerError, nil, errors.New("can't get info from context", http.StatusInternalServerError))
-		return
+		token, user, err := GetClaimsFromToken(c)
+		if err != nil {
+			response.JSON(c, "", err.Status, nil, err)
+			return
+		}
+		// TODO: check if token has not expired
+		// if the token has expired return a successful response
+		// if the token has not expired then store in the blacklist
+		accBlacklist := &models.BlackList{
+			Email: user.Email,
+			Token: token,
+		}
+		if err := s.AuthRepository.AddToBlackList(accBlacklist); err != nil {
+			log.Printf("can't add access token to blacklist: %v\n", err)
+			response.JSON(c, "logout failed", http.StatusInternalServerError, nil, errors.New("can't add access token to blacklist", http.StatusInternalServerError))
+			return
+		}
+		response.JSON(c, "logout successful", http.StatusOK, nil, nil)
 	}
 }
 
