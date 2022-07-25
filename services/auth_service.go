58b58811d536f3d734212740ead110ce793331c0
David Odohi <odohi.david@gmail.com>
2022-07-25 10:37:55 +0100

    do signup in the authservice file

--- services/auth_service.go
+++ services/auth_service.go
@@ -1,12 +1,105 @@
 package services
 
 import (
-	"github.com/golang-jwt/jwt"
+	"fmt"
+	"log"
+	"net/http"
 	"time"
+
+	"github.com/decagonhq/meddle-api/db"
+	"github.com/decagonhq/meddle-api/errors"
+	"github.com/decagonhq/meddle-api/models"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 const AccessTokenValidity = time.Hour * 24 * 7
 const RefreshTokenValidity = time.Hour * 24
+
+// AuthService interface
+type AuthService interface {
+	SignupUser(request *models.User) (*models.User, *errors.Error)
+}
+
+// authService struct
+type authService struct {
+	authRepo db.AuthRepository
+}
+
+// NewAuthService instantiate an authService
+func NewAuthService(authRepo db.AuthRepository) AuthService {
+	return &authService{
+		authRepo,
+	}
+}
+
+func (a *authService) SignupUser(user *models.User) (*models.User, *errors.Error) {
+	err := a.authRepo.IsEmailExist(user.Email)
+	if err != nil {
+		// FIXME: return the proper error message from the function
+		// TODO: handle internal server error later
+		return nil, errors.New("email already exist", http.StatusBadRequest)
+	}
+
+	err = a.authRepo.IsPhoneExist(user.PhoneNumber)
+	if err != nil {
+		return nil, errors.New("phone already exist", http.StatusBadRequest)
+	}
+
+	user.HashedPassword, err = GenerateHashPassword(user.Password)
+	if err != nil {
+		log.Printf("error generating password hash: %v", err.Error())
+		return nil, errors.New("internal server error", http.StatusInternalServerError)
+	}
+
+	user.IsEmailActive = false
+
+	user, err = a.authRepo.CreateUser(user)
+	if err != nil {
+		log.Printf("unable to create user: %v", err.Error())
+		return nil, errors.New("internal server error", http.StatusInternalServerError)
+	}
+	return user, nil
+}
+
+// GetTokenFromHeader returns the token string in the authorization header
+func GetTokenFromHeader(c *gin.Context) string {
+	authHeader := c.Request.Header.Get("Authorization")
+	if len(authHeader) > 8 {
+		return authHeader[7:]
+	}
+	return ""
+}
+
+// verifyAccessToken verifies a token
+func verifyToken(tokenString *string, claims jwt.MapClaims, secret *string) (*jwt.Token, error) {
+	parser := &jwt.Parser{SkipClaimsValidation: true}
+	return parser.ParseWithClaims(*tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(*secret), nil
+	})
+}
+
+// AuthorizeToken check if a refresh token is valid
+func AuthorizeToken(token *string, secret *string) (*jwt.Token, jwt.MapClaims, error) {
+	if token != nil && *token != "" && secret != nil && *secret != "" {
+		claims := jwt.MapClaims{}
+		token, err := verifyToken(token, claims, secret)
+		if err != nil {
+			return nil, nil, err
+		}
+		return token, claims, nil
+	}
+	return nil, nil, fmt.Errorf("empty token or secret")
+}
+
+func GenerateHashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashedPassword), err
+}
 
 // GenerateToken generates only an access token
 func GenerateToken(signMethod *jwt.SigningMethodHMAC, claims jwt.MapClaims, secret string) (string, error) {
