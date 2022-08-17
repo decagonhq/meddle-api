b7d9443731dd17e2d08924ae3de7a92693a677a1
iBoBoTi <iroegbucalistus@gmail.com>
2022-08-17 12:06:03 +0100

    feat: add the delete user by email feature

--- server/router.go
+++ server/router.go
@@ -31,6 +31,7 @@ func (s *Server) defineRoutes(router *gin.Engine) {
 	authorized.Use(s.Authorize())
 	authorized.POST("/logout", s.handleLogout())
 	authorized.GET("/users", s.handleGetUsers())
+	authorized.DELETE("/users", s.handleDeleteUser())
 	authorized.PUT("/me/update", s.handleUpdateUserDetails())
 	authorized.GET("/me", s.handleShowProfile())
 	authorized.POST("/user/medications", s.handleCreateMedication())
