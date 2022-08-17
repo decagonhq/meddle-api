45e3d32ad1762d2f9dca74a44d1cc0d6943420c1
toluwase <toluwasethomas1@gmail.com>
2022-08-17 08:40:39 +0100

    bearer

--- server/router.go
+++ server/router.go
@@ -29,7 +29,7 @@ func (s *Server) defineRoutes(router *gin.Engine) {
 
 	authorized := apirouter.Group("/")
 	authorized.Use(s.Authorize())
-	authorized.POST("/logout", s.handleLogout())
+	authorized.GET("/logout", s.handleLogout())
 	authorized.GET("/users", s.handleGetUsers())
 	authorized.PUT("/me/update", s.handleUpdateUserDetails())
 	authorized.GET("/me", s.handleShowProfile())
