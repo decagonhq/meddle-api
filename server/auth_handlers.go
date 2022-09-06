3d0c7799dcb2764d4d89d63a52613eda210d8133
iBoBoTi <iroegbucalistus@gmail.com>
2022-09-06 12:01:16 +0100

    doc: add the documentation for the facebook login

--- server/auth_handlers.go
+++ server/auth_handlers.go
@@ -180,8 +180,13 @@ func (s *Server) fbCallbackHandler() gin.HandlerFunc {
 			respondAndAbort(c, "", http.StatusUnauthorized, nil, errors.New("invalid authToken", http.StatusUnauthorized))
 			return
 		}
+		result := struct {
+			accessToken *string
+		}{
+			accessToken: authToken,
+		}
 
-		response.JSON(c, "facebook sign in successful", http.StatusOK, authToken, nil)
+		response.JSON(c, "facebook sign in successful", http.StatusOK, result, nil)
 	}
 }
 
