1e2b7a027012227730a2ee4c08ed03fb89335d15
iBoBoTi <iroegbucalistus@gmail.com>
2022-09-13 11:31:14 +0100

    feat: add the get all medication history and update medication history

--- models/medication_history.go
+++ models/medication_history.go
@@ -24,3 +24,31 @@ func NewMedicationHistory(medication Medication) *MedicationHistory {
 	}
 
 }
+
+type MedicationHistoryResponse struct {
+	ID                     uint   `json:"id"`
+	CreatedAt              string `json:"created_at"`
+	UpdatedAt              string `json:"updated_at"`
+	MedicationName         string `json:"medication_name"`
+	MedicationID           uint   `json:"medication_id"`
+	MedicationTime         string `json:"medication_time"`
+	MedicationDosage       int    `json:"medication_dosage"`
+	UserID                 uint   `json:"user_id"`
+	HasMedicationBeenTaken bool   `json:"has_medication_been_taken"`
+	WasMedicationMissed    string `json:"was_medication_missed"`
+}
+
+func (m *MedicationHistory) MedicationHistoryToResponse() *MedicationHistoryResponse {
+	return &MedicationHistoryResponse{
+		ID:                     m.ID,
+		CreatedAt:              time.Unix(m.CreatedAt, 0).String(),
+		UpdatedAt:              time.Unix(m.UpdatedAt, 0).String(),
+		MedicationName:         m.MedicationName,
+		MedicationID:           m.MedicationID,
+		MedicationTime:         m.MedicationTime.UTC().String(),
+		MedicationDosage:       m.MedicationDosage,
+		UserID:                 m.UserID,
+		HasMedicationBeenTaken: m.HasMedicationBeenTaken,
+		WasMedicationMissed:    m.WasMedicationMissed,
+	}
+}
